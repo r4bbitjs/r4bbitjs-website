d50b668539fa7862171cc2706073b8d83948f670
Baris Balli <barisbll.dev@gmail.com>
2023-09-02 12:28:35 +0300

    tailwind variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
   darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(var(--primary))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        codeLineBg: "hsl(var(--code-line-bg))",
+        themePrimary: "hsl(var(--theme-primary))",
+        themePrimaryDark: "hsl(var(--theme-primary-dark))",
+        themePrimaryDarker: "hsl(var(--theme-primary-darker))",
+        themePrimaryDarkest: "hsl(var(--theme-primary-darkest))",
+        themePrimaryLight: "hsl(var(--theme-primary-light))",
+        themePrimaryLighter: "hsl(var(--theme-primary-lighter))",
+        themePrimaryLightest: "hsl(var(--theme-primary-lightest))",
+      }
+    },
   },
   plugins: [],
 };
