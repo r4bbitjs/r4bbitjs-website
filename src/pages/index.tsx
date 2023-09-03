91c73b679c6bb08c1ca1066d81c6c88370656819
Baris Balli <barisbll.dev@gmail.com>
2023-09-03 18:14:07 +0300

    Changed layout for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const R4bbitLogo = require("@site/static/img/logo.svg").default;
 function HomepageHeader() {
   return (
     <div className="w-full h-[95vh] flex flex-col lg:flex-row lg:pb-20">
-      <div className="lg:w-1/2 h-2/3 lg:h-full flex justify-center lg:justify-end 2xl:justify-center items-center">
+      <div className="lg:w-1/2 h-3/5 lg:h-full flex justify-center lg:justify-end 2xl:justify-center items-center">
         <div className="min-h-min w-fit lg:w-full lg:max-w-[80%] 2xl:scale-150 md:max-w-none flex justify-center lg:justify-end">
           <img
             className=""
@@ -15,7 +15,7 @@ function HomepageHeader() {
           />
         </div>
       </div>
-      <div className="w-full lg:w-1/2  h-1/3 lg:h-full flex justify-center items-start lg:justify-start lg:items-center">
+      <div className="w-full lg:w-1/2  h-2/5 lg:h-full flex justify-center items-start lg:justify-start lg:items-center">
         <div className="w-full px-16 lg:px-0 lg:w-3/4 2xl:w-1/2 flex flex-col">
           <h1 className="text-4xl lg:text-7xl xl:text-8xl 2xl:text-[9rem] r4bbitBackgroundHeaderTitle mb-0">
             R4bbit
