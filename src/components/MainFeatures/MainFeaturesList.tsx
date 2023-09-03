5c334bb7c9ca17a72718c19f5e63d508103c66fb
Baris Balli <barisbll.dev@gmail.com>
2023-09-03 18:19:35 +0300

    Changed the main features padding on mobile

--- src/components/MainFeatures/MainFeaturesList.tsx
+++ src/components/MainFeatures/MainFeaturesList.tsx
@@ -5,7 +5,7 @@ import { MainFeatureInclusive } from "./MainFeatureInclusive";
 
 export const MainFeaturesList = (): JSX.Element => {
   return (
-    <div className="w-full flex flex-col items-center lg:flex-row justify-around px-16 lg:gap-6 mb-16">
+    <div className="w-full flex flex-col items-center lg:flex-row justify-around px-8 lg:gap-6 mb-16">
       <MainFeatureInclusive />
       <MainFeatureBatteriesIncluded />
       <MainFeatureSimple />
